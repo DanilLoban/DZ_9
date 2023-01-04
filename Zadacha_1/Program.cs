ff55acee07ea2963a89517e7771d7063d5b67d1f
Danil <dan.loban@yandex.ru>
2023-01-04 23:00:15 +0300

    fert

---
+++ Zadacha_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+int Numbers(int N)
+{
+    if(N < 0)
+    {
+        Console.Write("Не натуральное число");
+    }
+    if(N == 1)
+    {
+        return 1;
+    }
+    else 
+    {
+        Console.Write(N + " ");
+        return Numbers(N - 1);
+    }
+}
+Console.WriteLine(Numbers(20));
