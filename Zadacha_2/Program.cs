204b3c6f7a28e8bc20c7cc9f513d78c2ca2c668d
Danil <dan.loban@yandex.ru>
2023-01-05 22:36:07 +0300

    tgui

---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int Numbers(int M, int N)
+{
+    if(M < 0) Console.Write("Не натуральное число");
+    if(N < 0) Console.Write("Не натуральное число");
+    if(M >= N) return M;
+    else 
+    {
+        return M + Numbers(M + 1, N);
+    }
+}
+Console.WriteLine(Numbers(1, 10));
